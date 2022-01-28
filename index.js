955e0e31589dcd3186cdd7b7f9bfe5846f0d7992
Amoun82 <97534182+Amoun82@users.noreply.github.com>
2022-01-28 09:00:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.btn');
+
+ 
+
+
+btn.addEventListener('click', () => {
+    
+    //console.log("coucou") ;
+    //document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+})
